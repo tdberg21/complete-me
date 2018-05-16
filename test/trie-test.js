41a0c7a1110e5a16aaf64103fa32ab17aa6d81b4
Tory Dannenberg <rfdberg@gmail.com>
2018-05-16 15:05:32 -0600

    Add insertion and suggest functions and tests

--- test/trie-test.js
+++ test/trie-test.js
@@ -1,28 +1,55 @@
 import { assert } from 'chai';
+import { expect } from 'chai';
 import Trie from '../lib/Trie';
 import Node from '../lib/Node';
 
 describe('TRIE', () => {
-  let node;
-  let tree;
+  let trie;
 
   beforeEach(() => {
-    node = new Node();
-    tree = new Trie();
+    trie = new Trie();
   });
 
-  it('should return true', () => {
-    assert.equal(true, true);
+  it('should instantiate a new Trie', () => {
+    const expected = {
+      count: 0,
+      suggestionArray: [],
+      root: {
+        children: {},
+        data: null,
+        completedWord: null
+      }
+    };
+    const actual = trie;
+
+    assert.deepEqual(actual, expected);
   });
 
-  it('should be a function', () => {
-    assert.isFunction(Trie);
-  });
-  
-  it('should create a new Trie', () => {
-    insert('apple'); 
 
-    assert.isObject(node);
+  describe('INSERT', () => {
+    
+    it('should add nodes to the tree', () => {
+      trie.insert('dogs');
+      expect(Object.keys(trie.root.children)[0]).to.equal('d');
+    })
+    
   });
-  
+
+  describe('SUGGEST', () => {
+
+    it('should suggest words based on the prefix', () => {
+      let trie = new Trie();
+
+      trie.insert('dog');
+      trie.insert('double');
+      trie.insert('door');
+
+      trie.suggest('do');
+      console.log(JSON.stringify(trie, null, 4));
+
+      expect(trie.suggestionArray).to.deep.equal(['dog', 'double', 'door']);
+    });
+    
+  });
+
 });
